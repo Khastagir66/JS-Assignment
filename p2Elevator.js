a82ad85dfd4ec03d224a2fcc563494ec38d9dfe4
Khastagir66 <khastagirmech@gmail.com>
2026-07-30 01:16:04 +0600

    2nd question solve

---
+++ p2Elevator.js
@@ -0,0 +1,14 @@
+function isElevatorSafe(weights) {
+  let sumOfArray = 0;
+
+  if (Array.isArray(weights)) {
+    for (let elem of weights) {
+      sumOfArray += elem;
+    }
+    return sumOfArray <= 400;
+  } else {
+    return "Invalid";
+  }
+}
+
+// console.log(isElevatorSafe([400]));
